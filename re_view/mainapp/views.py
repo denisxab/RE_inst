a02d2528d17cec4c51307389443b15f96f420d09
“denis” <denis-kustov@rambler.ru>
2021-12-06 00:09:49 +0300

    6:0

--- re_view/mainapp/views.py
+++ re_view/mainapp/views.py
@@ -1,9 +1,8 @@
-import re
+from re import sub, search, split, match, findall
 
 from django.core.handlers.wsgi import WSGIRequest
 from django.http import JsonResponse
 from django.shortcuts import render
-# Create your views here.
 from django.views import View
 
 
@@ -11,43 +10,29 @@ class RePage(View):
 	template_name = "mainapp/re_page.html"  # Путь к шаблону `html`
 	http_method_names = ["get", "post", ]  # Список методов HTTP, которые обрабатывает класс.
 	
-	context_object_name = 're'
-	
-	# model =  # Какую модель используем
-	# queryset = .objects. # Получаем данные из БД
-	
+	# Словарь поддерживаемых регулярных функций
 	convert_srt_in_fun = {
-			"match"  : re.match,
-			"search" : re.search,
-			"split"  : re.split,
-			"sub"    : re.sub,
-			"findall": re.findall,
+			"match"  : match,
+			"search" : search,
+			"split"  : split,
+			"sub"    : sub,
+			"findall": findall,
 	}
 	
 	def get(self, request: WSGIRequest):
-		"""
-		В методе обрабатывается GET запрос
-		request.method == "GET"
-		"""
-		
 		return render(request,
 		              template_name=self.template_name,
-		              context={"list_re_fun": self.convert_srt_in_fun.keys()}
-		              )
+		              context={"list_re_fun": self.convert_srt_in_fun.keys()})
 	
 	def post(self, request: WSGIRequest, *args, **kwargs):
-		"""
-		В методе обрабатывается POST запрос
-		request.method == "POST"
-		"""
 		
 		# Регулярная функция
-		re_fun = self.convert_srt_in_fun.get(request.POST.get('flag_func_re', 'match'), re.match)
+		re_fun = self.convert_srt_in_fun.get(request.POST.get('flag_func_re', 'match'), match)
 		# Шаблон для регулярной функции
 		templates_re = request.POST.get('templates_re', None)
 		# Текст для регулярной функции
 		request_text_re = request.POST.get('data_from_form', None)
-		# Текст дл замены. Используется для регулярной функции `sub`
+		# Текст для замены. Используется для регулярной функции `sub`
 		replace_templates_re = request.POST.get('replace_templates_re', '')
 		
 		#  Если есть шаблон текста и регулярная функция.
